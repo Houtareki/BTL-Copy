5ca3ac4e2e94265ec4f7667d533218b12d222cdb
Houtareki <tadaylahaha@gmail.com>
2025-11-14 23:28:19 +0700

    test

--- src/main/java/com/example/demo/Controller/AdminController.java
+++ src/main/java/com/example/demo/Controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller;
 import com.example.demo.Model.CommonModel.UserForm;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +37,38 @@ public class AdminController {
         }
     }
 
-    // user
+    @GetMapping("/users")
+    @ResponseBody
+    public ResponseEntity<?> getUsers(
+            @RequestParam(name = "userId") int userId,
+            @RequestParam(name = "pageNo") int pageNo) {
+
+        if (isNotAdmin(userId)) {
+            return ResponseEntity.ok(new CustomResponse<>("Error", "Bạn không có quyền truy cập", null));
+        }
+
+        Page<User> usersPage = userService.getAllUsers(pageNo);
+        CustomData<Page<User>> data = new CustomData<>(usersPage);
+
+        return ResponseEntity.ok(new CustomResponse<>("Success", "Lấy danh sách user thành công", data));
+    }
+
+    @GetMapping("/search/users")
+    @ResponseBody
+    public ResponseEntity<?> searchUsers(
+            @RequestParam(name = "userId") int userId,
+            @RequestParam(name = "pageNo") int pageNo,
+            @RequestParam(name = "keyword") String keyword) {
+
+        if (isNotAdmin(userId)) {
+            return ResponseEntity.ok(new CustomResponse<>("Error", "Bạn không có quyền truy cập", null));
+        }
+
+        Page<User> usersPage = userService.searchUsers(keyword, pageNo);
+        CustomData<Page<User>> data = new CustomData<>(usersPage);
+
+        return ResponseEntity.ok(new CustomResponse<>("Success", "Tìm kiếm user thành công", data));
+    }
 
     @GetMapping("/user")
     @ResponseBody
@@ -55,11 +87,9 @@ public class AdminController {
         }
 
         CustomData<User> data = new CustomData<>(user);
-        CustomResponse<User> response = new CustomResponse<>("Success", "Lấy thành công", data);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new CustomResponse<>("Success", "Lấy thành công", data));
     }
 
-    // thêm user hoặc admin
     @PostMapping("/add/user")
     @ResponseBody
     public ResponseEntity<?> addUser(
@@ -76,11 +106,10 @@ public class AdminController {
             return ResponseEntity.ok(new CustomResponse<>("Error", "Tạo thất bại. Username hoặc email đã được sử dụng.", null));
         }
 
-        CustomResponse<User> response = new CustomResponse<>("Success", "Tạo user thành công", null);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new CustomResponse<>("Success", "Tạo user thành công", null));
     }
 
-    // cập nhật user
+    // 7. API Cập nhật User
     @PutMapping("/update/user")
     @ResponseBody
     public ResponseEntity<?> updateUser(
@@ -98,11 +127,10 @@ public class AdminController {
             return ResponseEntity.ok(new CustomResponse<>("Error", "Cập nhật thất bại!", null));
         }
 
-        CustomResponse<User> response = new CustomResponse<>("Success", "Cập nhật thành công", null);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new CustomResponse<>("Success", "Cập nhật thành công", null));
     }
 
-    //Xóa user
+    // 8. API Xóa User
     @DeleteMapping("/delete/user")
     @ResponseBody
     public ResponseEntity<?> deleteUser(
@@ -119,7 +147,6 @@ public class AdminController {
             return ResponseEntity.ok(new CustomResponse<>("Error", "Xóa thất bại!", null));
         }
 
-        CustomResponse<?> response = new CustomResponse<>("Success", "Xóa thành công", null);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new CustomResponse<>("Success", "Xóa thành công", null));
     }
 }
